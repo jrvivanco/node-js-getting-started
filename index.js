375d5455c46bff1dfaebf3d166c91f309868ac5b
jrvivanco <jrvivanco@gmail.com>
2017-01-20 19:15:50 -0600

    Ajustes para Mascotita

--- index.js
+++ index.js
@@ -25,19 +25,20 @@ app.get('/android', function(request, response) {
   response.render('pages/index');
 });
 
+
 //POST
 //https://calm-headland-74792.herokuapp.com/token-device
 //token
-//id_usuario_instagram
-var tokenDevicesURI = "usuario_instagram";
+//animal
+var tokenDevicesURI = "token-device";
 app.post('/' + tokenDevicesURI, function(request, response) {
-	var id_dispositivo 	= request.body.id_dispositivo;
-	var id_usuario_instagram 	= request.body.id_usuario_instagram;
+	var token 	= request.body.token;
+	var animal 	= request.body.animal;
 	var db = firebase.database();
 	var tokenDevices = db.ref(tokenDevicesURI).push();
 	tokenDevices.set({
-		id_dispositivo: id_dispositivo,
-		id_usuario_instagram: id_usuario_instagram
+		token: token,
+		animal: animal
 	});	
 
 	var path = tokenDevices.toString(); //https://mascotita-aa119.firebaseio.com/token-device/-KJlTaOQPwP-ssImryV1
@@ -50,6 +51,81 @@ app.post('/' + tokenDevicesURI, function(request, response) {
 });
 
 function generarRespuestaAToken(db, idAutoGenerado) {
+	var respuesta = {};
+	var usuario = "";
+	var ref = db.ref("token-device");
+	ref.on("child_added", function(snapshot, prevChildKey) {
+		usuario = snapshot.val();
+		respuesta = {
+			id: idAutoGenerado,
+			token: usuario.token,
+			animal: usuario.animal
+		};
+	});
+	return respuesta;
+}
+
+//GET
+//https://calm-headland-74792.herokuapp.com/toque-animal
+//id
+//animal
+app.get("/toque-animal/:id/:animal", function(request, response){
+	var id 		= request.params.id;
+	var animal 	= request.params.animal;
+
+	var db = firebase.database();
+	var ref = db.ref("token-device/" + id);
+	var usuario = "";
+	var respuesta = {};
+	
+	ref.on("value", function(snapshot) {
+		console.log(snapshot.val());
+		usuario = snapshot.val();
+		var mensaje = animal + " te dio un toque";
+		enviarNotificaion(usuario.token, mensaje);
+		respuesta = {
+			id: id,
+			token: usuario.token,
+			animal: usuario.animal
+		};
+		response.send(JSON.stringify(respuesta));
+	}, function (errorObject) {
+		console.log("The read failed: " + errorObject.code);
+		respuesta = {
+			id: "",
+			token: "",
+			animal: ""
+		};
+		response.send(JSON.stringify(respuesta));
+
+	});
+});
+
+//POST
+//https://calm-headland-74792.herokuapp.com/usuario_instagram
+//token
+//id_usuario_instagram
+var usuarioDevicesURI = "usuario_instagram";
+app.post('/' + usuarioDevicesURIDevicesURI, function(request, response) {
+	var id_dispositivo 	= request.body.id_dispositivo;
+	var id_usuario_instagram 	= request.body.id_usuario_instagram;
+	var db = firebase.database();
+	var usuarios = db.ref(usuarioDevicesURI).push();
+	usuarios.set({
+		id_dispositivo: id_dispositivo,
+		id_usuario_instagram: id_usuario_instagram
+	});	
+
+	var path = usuarios.toString(); //https://mascotita-aa119.firebaseio.com/token-device/-KJlTaOQPwP-ssImryV1
+	var pathSplit = path.split(usuarioDevicesURI + "/")
+	var idAutoGenerado = pathSplit[1];
+
+	var respuesta = generarRespuestaUsuario(db, idAutoGenerado);
+	response.setHeader("Content-Type", "application/json");
+	response.send(JSON.stringify(respuesta));
+});
+
+function generarRespuestaUsuario(db, idAutoGenerado) {
 	var respuesta = {};
 	var usuario = "";
 	var ref = db.ref("usuario_instagram");
